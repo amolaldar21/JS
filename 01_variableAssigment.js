4f4c0f23ff8352b24fd77869b4eb71a29fc1c87a
-u <-u>
2023-07-20 00:41:57 +0530

    variable declarations

---
+++ 01_variableAssigment.js
@@ -0,0 +1,26 @@
+var myFullName  // vairable declarations
+console.log(myFullName) // printing default values of variable
+var myFullName="Amol Aldar" // variable initialization
+console.log(myFullName) // print
+var myFullName="Amol Ishawar Aldar"
+console.log(myFullName)
+console.log("----------------------------------------------------------------")
+var myLove="I love only JS"
+console.log(myLove)
+console.log("--------------------------------------------------------------------")
+var firstName="Amol"
+var lastName="Aldar"
+var age=32
+var address="Sangole"
+var pinCode=410307
+var gender="Male"
+var isMarried="Yes"
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(address)
+console.log(pinCode)
+console.log(gender)
+console.log(isMarried)
+
+
