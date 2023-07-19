33747094fb542b3bd2036f8b6f3736a040dda346
-u <-u>
2023-07-19 08:13:31 +0530

    variable and hello js file

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+var myFirstName; // Variable Declaration
+myName = "Amol"; // Variable Assignment or Initialization
+
+console.log(myName);
+
+var city = "Navi Mumbai"; // Variable Declaration and Initialization
+console.log(city);
+
+var pin; // Variable Declaration
+console.log(pin);
