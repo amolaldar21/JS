8524612d8ad280ad5aaf576b9a984af3743e1e83
-u <-u>
2023-07-20 21:23:14 +0530

    swap program

---
+++ 02_datatypeAssigments.js
@@ -0,0 +1,26 @@
+var bankName 
+var marks='90%'
+console.log(typeof(bankName))
+console.log(bankName)
+var bankName="SBI Bank"
+console.log(typeof(bankName))
+console.log(bankName)
+console.log("----------------------------------------------------------------------------------------------------------------")
+console.log(marks)
+console.log(typeof(marks))
+console.log("----------------------------------------------------------------------------------------------------------------")
+var isWorking
+console.log(isWorking)
+var isWorking=true
+console.log(isWorking)
+console.log(typeof(isWorking))
+console.log("----------------------------------------------------------------------------------------------------------------")
+var totalCount="Hundred and Seven"
+console.log(totalCount)
+console.log(typeof(totalCount))
+var totalCount=107
+console.log(totalCount)
+console.log(typeof(totalCount))
+var a=null;
+console.log(a)
+console.log(typeof(a))
---
+++ swap.js
@@ -0,0 +1,26 @@
+var sweety ="Sweety"
+var cutie="Cutie"
+console.log("value of sweety before swap",sweety)
+console.log("value of cutie before swap",cutie)
+var temp
+temp=sweety
+sweety=cutie
+cutie=temp
+console.log("value of sweety after swap",sweety)
+console.log("value of cutie after swap" ,cutie)
+console.log("=================================================================================")
+// three variale
+var num1,num2,num3,temp
+num1=100
+num2=200
+num3=300
+console.log("value if num1 before swap",num1)
+console.log("value if num2 before swap",num2)
+console.log("value if num3 before swap",num3)
+temp=num1
+num1=num2
+num2=num3
+num3=temp
+console.log("value if num1 after swap",num1)
+console.log("value if num2 after swap",num2)
+console.log("value if num3 after swap",num3)
