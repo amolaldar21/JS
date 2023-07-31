3f7f44645e409f9d831a13472b3da9be155c29e0
-u <-u>
2023-07-31 21:19:53 +0530

    loop  assignments answer

---
+++ 06_flAssignments.js
@@ -0,0 +1,32 @@
+for (let index = 5; index <16; index++) {
+   console.log(index);
+}
+console.log("========================");
+for(i=50; i>=40; i--){
+    console.log(i);
+}
+console.log("========================");
+for(i=1; i<=30; i=i+2){
+    console.log(i)
+
+}
+console.log("========================");
+for(i=0;i<=20;i=i+2)
+{
+    console.log(i);
+}
+console.log("========================");
+for(i=1;i<11;i++){
+    console.log(i*5)
+
+}
+console.log("========================");
+for(i=1;i<11;i++){
+    console.log(i*10)
+
+}
+console.log("========================");
+for(i=10;i>=1;i--){
+    console.log(i*10)
+
+}
---
+++ 06_whileAssignments.js
@@ -0,0 +1,45 @@
+var i=5;
+while (i<16) {
+    console.log(i);
+    i++
+}
+console.log("========================");
+var i=50;
+while(i>=40){
+console.log(i);
+i--
+}
+console.log("========================");
+var i=1;
+while (i<=30){
+console.log(i);
+i=i+2
+}
+console.log("========================");
+var i=0;
+while (i<=20){
+console.log(i);
+i=i+2
+}
+console.log("========================");
+var i=5;
+while (i<11) {
+    console.log(i*5);
+    i=i+1
+    
+}
+
+console.log("========================");
+var i=1;
+while (i<11) {
+    console.log(i*10);
+    i=i+1
+    
+}
+console.log("========================");
+var i=10;
+while (i>=1) {
+    console.log(i*10);
+    i=i-1
+    
+}
